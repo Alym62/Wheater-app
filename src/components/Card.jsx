7ca3d9ef37d731433bd5271eedb4686348ce26ff
AlyM <gasafm62@gmail.com>
2023-08-13 16:08:37 -0300

    dpy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,33 +5,33 @@ import propTypes from 'prop-types';
 import React from 'react';
 
 function Card({ data }) {
-    const { location, current } = data;
+  const { location, current } = data;
 
-    return (
+  return (
 
-        <div className="bg-white p-6 mt-10 rounded-lg shadow-lg min-w-[250px]">
-            <div className="text-center">
-                <span className="block text-xl font-bold text-slate-800">{location.name}</span>
-                <span className="text-slate-500 text-sm font-medium">{`${location.region}, ${location.country}`}</span>
-            </div>
+    <div className="bg-white p-6 mt-10 rounded-lg shadow-lg min-w-[250px]">
+      <div className="text-center">
+        <span className="block text-xl font-bold text-slate-800">{location.name}</span>
+        <span className="text-slate-500 text-sm font-medium">{`${location.region}, ${location.country}`}</span>
+      </div>
 
-            <div className="font-bold flex mt-5 mb-2 justify-center">
-                <span className="text-6xl text-slate-800">{current.temp_c}</span>
-                <span className="text-xl text-slate-800 mt-2">°C</span>
-            </div>
+      <div className="font-bold flex mt-5 mb-2 justify-center">
+        <span className="text-6xl text-slate-800">{current.temp_c}</span>
+        <span className="text-xl text-slate-800 mt-2">°C</span>
+      </div>
 
-            <div className="flex justify-center flex-col items-center">
-                <img src={current.condition.icon} alt="icone" />
-                <span className="text-slate-800 font-medium">{current.condition.text}</span>
-            </div>
+      <div className="flex justify-center flex-col items-center">
+        <img src={current.condition.icon} alt="icone" />
+        <span className="text-slate-800 font-medium">{current.condition.text}</span>
+      </div>
 
-        </div>
+    </div>
 
-    );
+  );
 }
 
 export default Card;
 
 Card.propTypes = {
-    data: propTypes.object,
+  data: propTypes.object,
 }.isRequired;
--- src/services/api.js
+++ src/services/api.js
@@ -4,12 +4,13 @@ const KEY = '261e625a2eed4d4da87145325230403';
 // eslint-disable-next-line linebreak-style
 
 const fetchData = async (city) => {
-    const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+  // eslint-disable-next-line indent
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
 
-    const fetchResponse = await fetch(url);
-    // eslint-disable-next-line linebreak-style
-    const data = await fetchResponse.json();
-    return data;
+  const fetchResponse = await fetch(url);
+  // eslint-disable-next-line linebreak-style
+  const data = await fetchResponse.json();
+  return data;
 };
 
 export default fetchData;
